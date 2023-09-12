522c4c6d76bc4e7de9c53508769597d6025034d8
Hak-Vihet <hak.vichet19@kit.edu.kh>
2023-09-12 10:42:28 +0700

    update project

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -5,60 +5,79 @@ import { ProjectTypes } from "./types";
 export const cx = classNames;
 
 // nav data
-export const navData = [ "About", "Experience", "Works", "Contact" ]
+export const navData = ["About", "Experience", "Works", "Contact"];
 
 // project data
-export const projects : ProjectTypes[] = [
-    {
-        title : "Web3 Trading Bot",
-        description : "Personal Website V2 Second iteration of my personal website. Designed and developed with a conscious effort to avoid Personal Website V1 Second iteration of my personal website. and Also I add Blog section and saleing system",
-        tech : [ "React Js", "Material UI", "TypeScript", "Blockchain" ],
-        code : '',
-        live : 'https://web3-tradingbot.vercel.app/',
-        thumnail : "web3.png",
-        featured : true
-    },
+export const projects: ProjectTypes[] = [
+  {
+    title: "Web3 Trading Bot",
+    description:
+      "Personal Website V2 Second iteration of my personal website. Designed and developed with a conscious effort to avoid Personal Website V1 Second iteration of my personal website. and Also I add Blog section and saleing system",
+    tech: ["React Js", "Material UI", "TypeScript", "Blockchain"],
+    code: "",
+    live: "https://web3-tradingbot.vercel.app/",
+    thumnail: "web3.png",
+    featured: true,
+  },
 
-    {
-        title : "Proadvisor",
-        description : "'Dg team' an company management system. I use this system for front end “Next js, Tailwind CSS, Axios, Redux, Redux-thunk, Formik, Yup, react-query” and for Backend “Node js, Express js, MongoDB, Socket.io, webRtc”",
-        tech : [ "React js", "tailwind CSS", "Next Js", "Spring Boot", "TypeScript" ],
-        code : 'https://github.com/Hak-Vichet/Proadvisor',
-        live : 'https://proadvisors.vercel.app/',
-        thumnail : "proadvisor.png",
-        featured : true
-    },
+  {
+    title: "Proadvisor",
+    description:
+      "'Dg team' an company management system. I use this system for front end “Next js, Tailwind CSS, Axios, Redux, Redux-thunk, Formik, Yup, react-query” and for Backend “Node js, Express js, MongoDB, Socket.io, webRtc”",
+    tech: ["React js", "tailwind CSS", "Next Js", "Spring Boot", "TypeScript"],
+    code: "https://github.com/Hak-Vichet/Proadvisor",
+    live: "https://proadvisors.vercel.app/",
+    thumnail: "proadvisor.png",
+    featured: true,
+  },
 
-    {
-        title : "Smart Laundry Dashboard",
-        description : "Airebix is an web development agency website. ",
-        tech : [ "Figma", "React Js", "Tailwind CSS", "Mongodb", "Express JS" ],
-        live : '',
-        thumnail : "laundry.png",
-    },
-    {
-        title : "PomoDesk | Desktop Application",
-        description : "I clone World Largest E-commerce website Amazon using react js",
-        tech : [ "React js", "Tailwind CSS", "Figma" ],
-        code : '',
-        live : '',
-        thumnail : "pomo.png"
-    },
+  {
+    title: "Smart Laundry Dashboard",
+    description: "Airebix is an web development agency website. ",
+    tech: ["Figma", "React Js", "Tailwind CSS", "Mongodb", "Express JS"],
+    live: "",
+    thumnail: "laundry.png",
+  },
+  {
+    title: "PomoDesk | Desktop Application",
+    description:
+      "I clone World Largest E-commerce website Amazon using react js",
+    tech: ["React js", "Tailwind CSS", "Figma"],
+    code: "",
+    live: "",
+    thumnail: "pomo.png",
+  },
+  {
+    title: "Modern Bank | Website",
+    description: "I have build a responsive and modern bank leading page.",
+    tech: ["React js", "Tailwind CSS", "Figma"],
+    code: "https://github.com/Hak-Vichet/Modern_Bank.git",
+    live: "https://modern-bank-vichet.vercel.app/",
+    thumnail: "modern_bank.png",
+  },
+  {
+    title: "AI Summary | Website",
+    description: "I have build SaaS website for AI Summary",
+    tech: ["React js", "Tailwind CSS", "Redux", "Rapid Api"],
+    code: "https://github.com/Hak-Vichet/AI_summary.git",
+    live: "https://ai-summary-mu.vercel.app/",
+    thumnail: "ai_summary.png",
+  },
 
-    {
-        title : "Amazon clone",
-        description : "I clone World Largest E-commerce website Amazon using react js",
-        tech : [ "React js", "Redux" ],
-        // code : 'https://github.com/raihanhosen011/amazon-nw',
-        live : 'https://amazon-nw.vercel.app/',
-        thumnail : "amazon.png"
-    },
+  // {
+  //     title : "Amazon clone",
+  //     description : "I clone World Largest E-commerce website Amazon using react js",
+  //     tech : [ "React js", "Redux" ],
+  //     // code : 'https://github.com/raihanhosen011/amazon-nw',
+  //     live : 'https://amazon-nw.vercel.app/',
+  //     thumnail : "amazon.png"
+  // },
 
-    // {
-    //     title : "Powermall",
-    //     description : "This is an largest e-commerce website of bangladesh",
-    //     tech : [ "next js", "tailwind css", "MERN" ],
-    //     live : 'https://powermall.com',
-    //     thumnail : "powermall.png"
-    // }
-]
+  // {
+  //     title : "Powermall",
+  //     description : "This is an largest e-commerce website of bangladesh",
+  //     tech : [ "next js", "tailwind css", "MERN" ],
+  //     live : 'https://powermall.com',
+  //     thumnail : "powermall.png"
+  // }
+];
